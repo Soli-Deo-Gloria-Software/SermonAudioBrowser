d9828b2dd1d768ca5f794be5036b18c290bd8d02
Thomas Wood <thomas.matthew.wood@outlook.com>
2024-09-25 15:13:41 -0500

    ng update to 18

--- sa-client/src/app/app.module.ts
+++ sa-client/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SermonComponent } from './components/sermon/sermon.component';
 import { SermonListComponent } from './components/sermon-list/sermon-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { WaveformComponent } from './components/waveform/waveform.component';
@@ -13,24 +13,17 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { RouterModule } from '@angular/router';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    SermonComponent,
-    SermonListComponent,
-    WaveformComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    NgbModule,
-    FormsModule,
-    RouterModule.forRoot([]),
-    NgxSpinnerModule.forRoot({type: 'line-scale'})
-  ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        SermonComponent,
+        SermonListComponent,
+        WaveformComponent,
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        NgbModule,
+        FormsModule,
+        RouterModule.forRoot([]),
+        NgxSpinnerModule.forRoot({ type: 'line-scale' })], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
